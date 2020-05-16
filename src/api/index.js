7725e0504cfcb4fc796877ce143b9c3b096c9212
Herun Zhou <iamherunzhou@gmail.com>
2020-05-16 17:30:42 -0400

    built out initial card structure

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,14 @@
 import axios from 'axios';
 
-const url = 'h'
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async () => {
+    try {
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+
+        return { confirmed, recovered, deaths, lastUpdate }
+
+    } catch (error) {
+
+    }
+}
